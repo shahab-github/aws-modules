1960725070b0386e4db6a496740470cec805d379
shahab-github <shahabuddinm679+github@gmail.com>
2023-08-06 14:55:15 -0500

    "added main.tf for Sec grp"

---
+++ security-groups/main.tf
@@ -0,0 +1,36 @@
+resource "aws_security_group" "web-sg" {
+  name = var.sg_name
+  description = "Allows http and https traffic"
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = var.sg_name
+  }
+}
