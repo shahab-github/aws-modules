e871994e4f8651155b79d225bed0ecb4f835e5d3
shahab-github <113800863+shahab-github@users.noreply.github.com>
2023-08-15 12:17:08 -0500

    added policy

--- s3/bucket-policy.tf
+++ s3/bucket-policy.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket_policy" "allow_public_read" {
-  bucket = aws_s3_bucket.example.id
+  bucket = aws_s3_bucket.s3_bucket.id
   policy = jsonencode({
     Version = "2012-10-17" 
     Statement = [
@@ -9,8 +9,8 @@ resource "aws_s3_bucket_policy" "allow_public_read" {
         Principal = "*"
         Action    = "s3:GetObject"
         Resource = [
-          aws_s3_bucket.example.arn,
-          "${aws_s3_bucket.example.arn}/*"
+          aws_s3_bucket.s3_bucket.arn,
+          "${aws_s3_bucket.s3_bucket.arn}/*"
         ]
       }
     ]
